420343e56e9a0455674ad79ec0d563bf3fa05c4d
Annson <21959423+Fiestaman@users.noreply.github.com>
2023-04-16 12:43:04 -0500

    add About page

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+export default function About() {
+  return (
+    <>
+      <div className="about">
+        <p>About Page</p>
+        <p>Created by Annson Cook</p>
+        <p>Pokemon&#169; is a registered trademark of Nintendo&#169;.</p>
+      </div>
+    </>
+  );
+}
