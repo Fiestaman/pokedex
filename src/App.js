351af978b71ba7b59cad1e4f881d55bf77442de6
Annson <21959423+Fiestaman@users.noreply.github.com>
2023-04-16 12:52:40 -0500

    update routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import { Route, Routes } from "react-router-dom";
 import PokemonList from "./pages/PokemonList";
 import Pokemon from "./pages/Pokemon";
+import Main from "./pages/Main";
+import About from "./pages/About";
+import { useState } from "react";
 
 function App() {
+  const [pokemonList, setPokemonList] = useState([]);
+
   return (
     <div className="App">
       <Routes>
-        <Route path="/" />
-        <Route path="/pokemons" element={<PokemonList />} />
+        <Route path="/" element={<Main />} />
+        <Route
+          path="/pokemons"
+          element={
+            <PokemonList
+              pokemonList={pokemonList}
+              setPokemonList={setPokemonList}
+            />
+          }
+        />
         <Route path="/pokemons/:id" element={<Pokemon />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </div>
   );
