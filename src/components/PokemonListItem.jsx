e5978cb2d7b6c6acaae1ac9954ba5b76893da33a
Annson <21959423+Fiestaman@users.noreply.github.com>
2023-04-17 12:48:26 -0500

    add collected functionality

--- src/components/PokemonListItem.jsx
+++ src/components/PokemonListItem.jsx
@@ -2,18 +2,35 @@ import { useNavigate } from "react-router-dom";
 
 export default function PokemonListItem({
   pokemon: { name, url, id, collected, img },
+  pokemonList,
+  setPokemonList,
 }) {
   const navigate = useNavigate();
 
-  const handleClick = () => {
-    navigate(`/pokemons/${id}`);
+  const handleClickPokemon = (e) => {
+    if (e.target.type !== "checkbox") {
+      navigate(`/pokemons/${id}`);
+    }
+  };
+
+  const handleCollected = () => {
+    collected = !collected;
+    pokemonList[id - 1].collected = collected;
+    setPokemonList([...pokemonList]);
   };
 
   return (
-    <div className="pokemonListItem" onClick={handleClick} key={id}>
-      <img src={img} alt={name} />
+    <div
+      className={`pokemonListItem${collected ? " collected" : ""}`}
+      onClick={handleClickPokemon}
+      key={id}
+    >
+      <div
+        className="pokemonImage"
+        style={{ backgroundImage: `url(${img})` }}
+      ></div>
       {id + ". " + name}
-      <input type="checkbox" checked={collected} />
+      <input type="checkbox" checked={collected} onChange={handleCollected} />
     </div>
   );
 }
