e7efefce77c26218a1762d0b664085050dc56a3e
Annson <21959423+Fiestaman@users.noreply.github.com>
2023-04-18 17:26:13 -0500

    add pull info on app load

--- src/App.js
+++ src/App.js
@@ -4,9 +4,16 @@ import Pokemon from "./pages/Pokemon";
 import Main from "./pages/Main";
 import About from "./pages/About";
 import { useState } from "react";
+import GetPokemons from "./components/GetPokemons";
+import Berries from "./pages/Berries";
+import Items from "./pages/Items";
+import GetBerries from "./components/GetBerries";
+import GetItems from "./components/GetItems";
 
 function App() {
   const [pokemonList, setPokemonList] = useState([]);
+  const [berriesList, setBerriesList] = useState([]);
+  const [itemsList, setItemsList] = useState([]);
 
   return (
     <div className="App">
@@ -22,9 +29,35 @@ function App() {
             />
           }
         />
-        <Route path="/pokemons/:id" element={<Pokemon key="Pokemon" />} />
+        <Route
+          path="/pokemons/:id"
+          element={<Pokemon key="Pokemon" pokemonList={pokemonList} />}
+        />
+        <Route
+          path="/berries"
+          element={<Berries key="Berries" berriesList={berriesList} />}
+        />
+        <Route path="/items" element={<Items itemsList={itemsList} />} />
         <Route path="/about" element={<About key="About" />} />
       </Routes>
+      {pokemonList.length === 0 ? (
+        <GetPokemons
+          pokemonList={pokemonList}
+          setPokemonList={setPokemonList}
+        />
+      ) : (
+        ""
+      )}
+      {berriesList.length === 0 ? (
+        <GetBerries berriesList={berriesList} setBerriesList={setBerriesList} />
+      ) : (
+        ""
+      )}
+      {itemsList.length === 0 ? (
+        <GetItems itemsList={itemsList} setItemsList={setItemsList} />
+      ) : (
+        ""
+      )}
     </div>
   );
 }
