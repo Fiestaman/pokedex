acde8397fe560c39ffd3cd20e8cc01604e89019e
Annson <21959423+Fiestaman@users.noreply.github.com>
2023-04-18 17:30:11 -0500

    clean up unused code

--- src/components/GetPokemons.jsx
+++ src/components/GetPokemons.jsx
@@ -2,7 +2,7 @@ import { gql, useQuery } from "@apollo/client";
 
 export default function GetPokemons({ pokemonList, setPokemonList }) {
   const queryPokemons = gql`
-    query samplePokeAPIquery {
+    {
       results: pokemon_v2_pokemon(limit: 905, order_by: { id: asc }) {
         id
         name
@@ -29,11 +29,6 @@ export default function GetPokemons({ pokemonList, setPokemonList }) {
     }
   `;
 
-  // if (pokemonList.length > 0) return;
-  // try {
-  //   const { data } = useQuery(queryPokemons);
-  //   console.log(data);
-
   const { loading, error, data } = useQuery(queryPokemons, {
     onCompleted: (completedData) => {
       let { results } = completedData;
@@ -42,43 +37,19 @@ export default function GetPokemons({ pokemonList, setPokemonList }) {
         let tempResult = { ...results[i] };
         tempResult.name =
           tempResult.name[0].toUpperCase() + tempResult.name.slice(1);
-        // data.results[i].id = i + 1;
         tempResult.collected = false;
-        tempResult.img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-          i + 1
-        }.png`;
-        // i < 151
-        //   ? (data.results[i].generation = 1)
-        //   : i < 251
-        //   ? (data.results[i].generation = 2)
-        //   : i < 386
-        //   ? (data.results[i].generation = 3)
-        //   : i < 493
-        //   ? (data.results[i].generation = 1)
-        //   : i < 649
-        //   ? (data.results[i].generation = 5)
-        //   : i < 721
-        //   ? (data.results[i].generation = 6)
-        //   : i < 809
-        //   ? (data.results[i].generation = 7)
-        //   : (data.results[i].generation = 8);
+        let shorturl =
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+        tempResult.img = `${shorturl}${i + 1}.png`;
+        tempResult.gif = `${shorturl}other/showdown/${i + 1}.gif`;
         tempResults.push(tempResult);
       }
       setPokemonList([...pokemonList, ...tempResults]);
-      // onCompleted is a standard useQuery option
-      //   setPokemonList(completedData.results);
     },
   });
 
-  if (loading) return "Loading...";
+  // if (loading) return "Loading...";
   if (error) return `Error! ${error.message}`;
-  //   console.log(data);
-  //   // const response = await fetch(url);
-  //   // const data = await response.json();
-  //   let { results } = data;
-  //   console.log("this is results", results);
+
   return;
-  // } catch (err) {
-  // console.log(err);
 }
-// };
