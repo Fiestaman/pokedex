edca664c813d213e85de8a2a2e6433cabb114285
Annson <21959423+Fiestaman@users.noreply.github.com>
2023-04-18 17:25:47 -0500

    add back button

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,10 @@
+import BackButton from "../components/BackButton";
+
 export default function About() {
   return (
     <>
       <div className="about">
+        <BackButton />
         <p>About Page</p>
         <p>Created by Annson Cook</p>
         <p>Pokemon&#169; is a registered trademark of Nintendo&#169;.</p>
